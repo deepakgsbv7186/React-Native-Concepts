65936e1a2c6d60df826a21063a74f2b66e25e315
@ppy <deekgsbv@gmail.com>
2023-09-09 20:24:13 +0530

    #added: appname, appicon, splash screen

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import Routes from './navigation/Routes';
 import {SafeAreaView} from 'react-native';
+import SplashScreen from 'react-native-splash-screen';
 
 export default function App() {
+  useEffect(()=>{
+    SplashScreen.hide()
+  },[])
   return (
     <>
       <SafeAreaView style={{flex: 1, backgroundColor: 'white'}}>
